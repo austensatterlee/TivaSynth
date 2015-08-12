made fixed-point scaling more explicit

genTables.py
@@ -1,22 +1,24 @@
 import numpy as np
-from numpy import linspace,logspace,arange,int32
+from numpy import linspace,logspace,arange,uint32,int32
 from numpy import log2,sin,pi,floor,ceil
 
 FS = 192000
 MOD_FS = FS>>8
-Q = 31
 TABLESIZE = 256
+Q = {
+        'osc.freqs':31,
+    }
 
 class Table:
-    def __init__(self,table,name,longname="",commentinfo={},cellfmt='#08x'):
+    def __init__(self,table,q,name,longname="",commentinfo={},cellfmt='#08x'):
         self.longname = longname
         self.table = table
         self.size = len(table)
         self.name = name
-        self.ctype = 'q31_t' if Q==31 else 'q15_t'
+        self.ctype = 'q31_t'
         self.cellfmt = cellfmt
         self.commentinfo = commentinfo
-        self.commentinfo.update({'FS':FS,'MOD_FS':MOD_FS,'Q':Q})
+        self.commentinfo.update({'FS':FS,'MOD_FS':MOD_FS,'Q':q})
         self.comment = """/*
  * {}
  *""".format(self.longname)
@@ -48,25 +50,15 @@ class Table:
                 break
         return showstr
 
-def GenEnvAlphas(t0,t1,tablesize=None,res=None):
-    envfs       = FS*1.0/MOD_FS
-    mintimeres  = 1.0/envfs
-    t0 = max(t0,mintimeres)
-    if tablesize:
-        timeres = max(mintimeres,float(t1-t0)/tablesize)
-    elif res:
-        timeres = max(mintimeres,res)
-    else:
-        timeres = float(t1-t0)/TABLESIZE
-    tablesize = int(float(t1-t0)/timeres)
-    times     = linspace(t0,t1,tablesize)
-
-    alphaTable = np.int32(0.05**(1./np.floor(envfs*times))*(1<<31))
-    return Table(alphaTable,
-            'envDecayTable',
-            'Envelope decay map',
-            {'t0':t0,'t1':t1}
-            )
+def GenEnvTables():
+    t = linspace(0,1,TABLESIZE,endpoint=False)
+    q = 31
+    softTable = int32(11*t/(t+10)*(1<<q))
+    return [Table(softTable,
+            q,
+            'SoftTable',
+            'Soft curve shape',
+            ),]
 
 def GenFilterCutoffs(f0,f1,tablesize=None,res=None):
     f1 = min(f1,FS>>1)
@@ -78,6 +70,7 @@ def GenFilterCutoffs(f0,f1,tablesize=None,res=None):
     F = logspace(log2(f0),log2(f1),tablesize,base=2,endpoint=False)
     fcTable = int32(sin(2*pi*F/FS)*(1<<31))
     return Table(fcTable,
+            (1,31),
             'fcTable',
             'Filter cutoff map',
             {'f0':f0,'f1':f1,}
@@ -86,9 +79,11 @@ def GenFilterCutoffs(f0,f1,tablesize=None,res=None):
 def GenOscFreqs(f0,tablesize=None):
     if not tablesize:
         tablesize = TABLESIZE
+    q = Q['osc.freqs']
     freqs = f0*(2**(arange(0,tablesize)/12.0))
-    freqTable = int32(freqs*(1<<31)/FS)
+    freqTable = int32(freqs*(1<<q)/FS)
     return Table(freqTable,
+            q,
             'noteTable',
             'Oscillator phase step map (notes)',
             {'f0':f0}
@@ -97,10 +92,12 @@ def GenOscFreqs(f0,tablesize=None):
 def GenOscSteps(tablesize=None):
     if not tablesize:
         tablesize = TABLESIZE
-    freqsteps = 2**(arange(-tablesize/2,tablesize/2)/12.0)
-    freqstepTable = int32(freqsteps*(1<<31)/FS)
+    q = Q['osc.freqs']/2
+    freqsteps = np.power(2.0,arange(-tablesize/2,tablesize/2)*1.0/12.0)
+    freqstepTable = np.uint32(freqsteps*(1<<q))
     return Table(
             freqstepTable,
+            q,
             'semitoneTable',
             'Oscillator phase step map (semitones)',
             )
@@ -112,11 +109,12 @@ if __name__=='__main__':
     args=parser.parse_args()
     tablecfile = "#include \"arm_math.h\"\n"
     tables=[
-    GenEnvAlphas(0.005,5.0),
-    GenFilterCutoffs(100,80000),
-    GenOscFreqs(440*2**(-14)),
-    GenOscSteps(),
+    GenFilterCutoffs(100,50000),
+    GenOscFreqs(440*2.**(-7),128),
+    GenOscSteps(128),
     ]
+    tables.extend(GenEnvTables())
+
     for table in tables:
         tablecfile+="\n"+table.showc()
     with open(args.outfile,'w') as outfp: